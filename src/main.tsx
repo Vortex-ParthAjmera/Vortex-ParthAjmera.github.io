d095636224d126c0728fdc6617676dd24c6cd013
Parth Ajmera <ajmeraparth.official@gmail.com>
2026-06-10 15:43:51 +0530

    code commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-// Fix GitHub Pages SPA redirect
+// Fix GitHub Pages 404 on refresh for SPAs
 const redirect = sessionStorage.redirect;
 if (redirect) {
   sessionStorage.removeItem('redirect');
   window.history.replaceState(null, '', redirect.replace(window.location.origin, ''));
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
